44883d5703cd1a7766c98a9cbe9cf7fe21a3a2af
Okia02 <anajimenezmelero@gmail.com>
2022-01-04 13:03:42 +0100

    add new deploy for gitHub pages

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,20 +3,18 @@
 const btn = document.querySelector(".js-input");
 const optionSelect = document.querySelector(".js_select_option");
 const compScore = document.querySelector(".js-attempts-comp");
-const userScore = document.querySelector('.js-attempts-user');
+const userScore = document.querySelector(".js-attempts-user");
 const resultMessage = document.querySelector(".js-result-message");
-let computerValue = '';
-let game = '';
-let attemptsCounter = document.querySelector('.js-attempts-counter');
-
-
+let attemptsCounter = document.querySelector(".js-attempts-counter");
+let compCounter = 0;
+let userCounter = 0;
+let counterGame = 0;
 function getRandomNumber() {
   return Math.ceil(Math.random() * 100);
 }
-
-
-
-function computerChoice () {
+function computerChoice() {
+  let computerValue = "";
+  let game = "";
   let randomNumber = getRandomNumber(100);
   let userChoice = optionSelect.value;
   if (randomNumber <= 33) {
@@ -27,60 +25,50 @@ function computerChoice () {
     computerValue = "tijera";
   }
   if (computerValue === userChoice) {
-    game = 'Empate &#128581';
-  } else if (computerValue === 'piedra') {
-    if (userChoice === 'papel') {
-      game = 'Has ganado &#127881';
+    game = "Empate &#128581";
+  } else if (computerValue === "piedra") {
+    if (userChoice === "papel") {
+      game = "Has ganado &#127881";
       userWins();
-    } else if (userChoice === 'tijera') {
-      game = 'Has perdido &#128128';
+    } else if (userChoice === "tijera") {
+      game = "Has perdido &#128128";
       compWins();
     }
-  } else if (computerValue === 'papel') {
-    if (userChoice === 'piedra') {
-      game = 'Has perdido &#128128';
+  } else if (computerValue === "papel") {
+    if (userChoice === "piedra") {
+      game = "Has perdido &#128128";
       compWins();
-    } else if (userChoice === 'tijera') {
-      game = 'Has ganado &#127881';
+    } else if (userChoice === "tijera") {
+      game = "Has ganado &#127881";
       userWins();
     }
-  } else if (computerValue === 'tijera') {
-    if (userChoice === 'piedra') {
-      game = 'Has ganado &#127881';
+  } else if (computerValue === "tijera") {
+    if (userChoice === "piedra") {
+      game = "Has ganado &#127881";
       userWins();
-    } else if (userChoice === 'papel') {
-      game = 'Has perdido &#128128';
+    } else if (userChoice === "papel") {
+      game = "Has perdido &#128128";
       compWins();
     }
-  }  
+  }
   resultMessage.innerHTML = `${game}`;
-  console.log(computerValue);
 }
-
-
- let compCounter = 0;
-function compWins() {   
+function compWins() {
   compCounter++;
   compScore.innerHTML = `${compCounter}`;
-  console.log('computadora');
 }
-  let userCounter = 0;
-function userWins() {  
+function userWins() {
   userCounter++;
   userScore.innerHTML = `${userCounter}`;
-  console.log('jugadora');
 }
- let counterGame = 0;
-function counter() { 
+function counter() {
   counterGame++;
   attemptsCounter.innerHTML = `${counterGame}`;
-  
 }
-
 function handlerBtn(ev) {
   ev.preventDefault();
   computerChoice();
-  counter();  
+  counter();
 }
 btn.addEventListener("click", handlerBtn);
 
